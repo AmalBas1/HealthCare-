12d93f5d2899052930fb48fd808af516a66ffe71
Amal <amalbasbas55@gmail.com>
2026-04-27 15:24:06 +0100

    feat: implement PatientController

--- src/main/java/com/healthcare/medical_system/controller/PatientController.java
+++ src/main/java/com/healthcare/medical_system/controller/PatientController.java
@@ -1,4 +1,58 @@
 package com.healthcare.medical_system.controller;
 
+import com.healthcare.medical_system.dto.PatientDTO;
+import com.healthcare.medical_system.entity.Patient;
+import com.healthcare.medical_system.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.ServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/patients")
 public class PatientController {
+    private final PatientService patientService;
+
+    @PostMapping()
+    @Operation(summary = "ajouter un patient")
+    public ResponseEntity<PatientDTO> ajoutePatient(@RequestBody PatientDTO patientDTO){
+        PatientDTO patientAjoute = patientService.ajouterPatient(patientDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(patientAjoute);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "modifier patient")
+    public ResponseEntity<PatientDTO> modifierPatient(@PathVariable Long id, @RequestBody PatientDTO patientDTO){
+        PatientDTO updatedPatient = patientService.ModifierPatient(id, patientDTO);
+        return ResponseEntity.ok(updatedPatient);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "supprimer patient")
+    public ResponseEntity<Void> supprimerPatient(@PathVariable Long id){
+        patientService.supprimerPatient(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    @Operation(summary = "liste de tous les patients")
+    public ResponseEntity<List<PatientDTO>> listerPatients(ServletRequest servletRequest){
+        List<PatientDTO> patients = patientService.listerPatients();
+        return ResponseEntity.ok(patients);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "consulter patient par id")
+    public ResponseEntity<PatientDTO> consulterPatient(@PathVariable Long id){
+        PatientDTO patient = patientService.consulterPatient(id);
+        return ResponseEntity.ok(patient);
+    }
+
+
 }
