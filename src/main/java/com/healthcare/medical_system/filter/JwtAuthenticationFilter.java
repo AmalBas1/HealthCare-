098566fb910175bd355354dda18e6d17b311f4e6
Amal <amalbasbas55@gmail.com>
2026-05-19 15:12:22 +0100

    add roles to authentication filter

--- src/main/java/com/healthcare/medical_system/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/healthcare/medical_system/filter/JwtAuthenticationFilter.java
@@ -1,19 +1,20 @@
 package com.healthcare.medical_system.filter;
 
 import com.healthcare.medical_system.config.JwtUtils;
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import java.util.Collections;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
@@ -26,29 +27,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        try {
-            String authHeader = request.getHeader("Authorization");
 
-            if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                String token = authHeader.substring(7);
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            if (jwtUtils.isTokenValid(token)) {
 
                 String username = jwtUtils.extractUsername(token);
+                String role = jwtUtils.extractRole(token);
 
                 if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                    if (jwtUtils.isTokenValid(token)) {
-                        UsernamePasswordAuthenticationToken auth =
-                                new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>());
-                        SecurityContextHolder.getContext().setAuthentication(auth);
-                    }
-                }
-            }
-            filterChain.doFilter(request, response);
 
-        } catch (ExpiredJwtException e) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
-            response.setContentType("application/json");
-            response.getWriter().write("{\"message\": \"Session expirée, merci de vous reconnecter.\"}");
+                    List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+role));
+
+                    UsernamePasswordAuthenticationToken auth =
+                            new UsernamePasswordAuthenticationToken(username, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }else{
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.setContentType("application/json");
+                response.getWriter().write("{\"message\": \"Session expirée ou token invalide, merci de vous reconnecter.\"}");
+                return;
+            }
+
         }
+        filterChain.doFilter(request, response);
+
     }
 
 }
